ec5ad2c5fa70a151e480ab4b473e1b03ff78e70e
Gonzalo Mahserdjian <128072781+gsmx64@users.noreply.github.com>
2024-08-28 00:54:27 +0000

    Added workflow azure new app deploy and a little style change on Hour React component

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -4,12 +4,16 @@ const Hours = () => {
 
     return (
         <div style={{marginBottom:'10px'}}>
-            <span>
-                10 a.m. to 4 p.m. Monday to Friday
-            </span>
-            <span>
-                9 a.m. to 8 p.m. Saturday and Sunday
-            </span>
+            <div>
+                <span>
+                    10 a.m. to 4 p.m. Monday to Friday
+                </span>
+            </div>
+            <div>
+                <span>
+                    9 a.m. to 8 p.m. Saturday and Sunday
+                </span>
+            </div>
         </div>
     )
 }
